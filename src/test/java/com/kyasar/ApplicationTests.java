a4c081fdbe0ebc48b46b41ff7ca08735479c5ccd
Kadir Yasar <kyasar.mail@mail.com>
2016-12-26 23:21:22 +0300

    Some test cases added.

--- src/test/java/com/kyasar/ApplicationTests.java
+++ src/test/java/com/kyasar/ApplicationTests.java
@@ -3,7 +3,11 @@ package com.kyasar;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +17,19 @@ public class ApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void getProducts()
+	{
+		final String uri = "http://localhost:8080/product";
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+
+		System.out.println(result);
+	}
 }
